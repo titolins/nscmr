c7b98238249a71f2c580b356b440075b15eda685
Tito Lins <tito@blinx.com.br>
2016-03-04 17:49:54 -0300

    translated password check text

--- nscmr/static/js/registration.js
+++ nscmr/static/js/registration.js
@@ -2,9 +2,9 @@ function checkPassword() {
   var password = $("#password-txt").val();
   var confirmPassword = $("#confirm-password").val();
   if (password != confirmPassword) {
-    $("#check-passwords-match").html("Passwords do not match!");
+    $("#check-passwords-match").html("As senhas não são iguais!");
   } else {
-    $("#check-passwords-match").html("Passwords match.");
+    $("#check-passwords-match").html("As senhas são iguais.");
   }
 };
 
