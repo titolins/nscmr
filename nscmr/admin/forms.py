610cb28d5577fd46c145bbb8dc5c601fd5c41d2d
Tito Lins <tito@blinx.com.br>
2016-03-17 15:52:30 -0300

    create redirect form as base form

---
+++ nscmr/admin/forms.py
@@ -0,0 +1,43 @@
+from urllib.parse import urlparse, urljoin
+from flask import request, url_for, redirect
+from flask.ext.wtf import Form
+from wtforms import TextField, HiddenField
+
+
+def is_safe_url(target):
+    '''
+    Method for checking url safety (used in connection with next redirect)
+    '''
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ('http', 'https') and \
+           ref_url.netloc == test_url.netloc
+
+
+def get_redirect_target():
+    '''
+    Get the redirect target for the given request
+    '''
+    for target in request.args.get('next'), request.referrer:
+        if not target:
+            continue
+        if is_safe_url(target):
+            return target
+
+
+class RedirectForm(Form):
+    '''
+    Base redirect form
+    '''
+    next = HiddenField()
+
+    def __init__(self, *args, **kwargs):
+        Form.__init__(self, *args, **kwargs)
+        if not self.next.data:
+            self.next.data = get_redirect_target() or ''
+
+    def redirect(self, endpoint='index', **values):
+        if is_safe_url(self.next.data):
+            return redirect(self.next.data)
+        target = get_redirect_target()
+        return redirect(target or url_for(endpoint, **values))
