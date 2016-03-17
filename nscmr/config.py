98d24c902e7cf79176e592f67e39cc243b9b5ff5
Tito Lins <tito@blinx.com.br>
2016-03-16 23:28:42 -0300

    add db config

--- nscmr/config.py
+++ nscmr/config.py
@@ -2,17 +2,21 @@ class Config(object):
     DEBUG = False
     TESTING = False
 
-    # update this when db is ready
-    # DATABASE_URI = ""
+    MONGO_HOST = 'localhost'
+    MONGO_PORT = 27017
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MONGO_DB = 'nscmr_dev'
+
+
+class TestingConfig(Config):
+    TESTING = True
+    MONGO_DB = 'nscmr_test'
+
 
 class ProductionConfig(Config):
     # update this when db is ready
     # DATABASE_URI = ""
-    pass
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-
-class TestingConfig(Config):
-    TESTING = True
-
+    MONGO_DB = 'nscmr'
