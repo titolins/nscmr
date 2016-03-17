fe4a2eefa84b5c824325b22920fc0f10e5feb2d0
Tito Lins <tito@blinx.com.br>
2016-03-17 16:30:05 -0300

    change login_form name

--- nscmr/forms.py
+++ nscmr/forms.py
@@ -5,7 +5,8 @@ from wtforms import (
     TextField,
     PasswordField,
     SubmitField,
-    ValidationError
+    ValidationError,
+    DateField
 )
 
 from wtforms.validators import input_required, email
@@ -20,3 +21,14 @@ class LoginForm(RedirectForm):
     password = PasswordField('senha', [input_required("Campo necessário!")])
 
 
+class RegistrationForm(RedirectForm):
+    name = TextField('name', validators=[input_required("Campo necessário!")])
+    email = TextField(
+        'email',
+        validators=[
+            input_required("Campo necessário!"),
+            email("Email inválido!")])
+    dob = DateField(format='%d/%m/%Y')
+    password = PasswordField('senha',[input_required("Campo necessário!")])
+    confirm_password = PasswordField(
+            'senha',[input_required("Campo necessário!")])
