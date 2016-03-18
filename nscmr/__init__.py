a08ef424e423cc5c58d7609927a44154bd0f6ef9
Tito Lins <tito@blinx.com.br>
2016-03-17 22:22:11 -0300

    create login manager

--- nscmr/__init__.py
+++ nscmr/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
+from flask.ext.login import LoginManager
+
 from instance.config import config_app
 from nscmr.admin.database import build_db
 
@@ -13,8 +15,14 @@ def build_app():
     app.register_blueprint(admin.bp, url_prefix='/admin')
     # config app and return
     config_app(app)
+    # starts Flask-Login
+    login_manager.init_app(app)
+
     return app
 
+# instantiates the login_manager outside the build_app method to allow us to
+# import it later..
+login_manager = LoginManager()
 app = build_app()
 db = build_db(app)
 
