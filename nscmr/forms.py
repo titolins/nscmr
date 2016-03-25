c0ea487c54c109a58b07bd3072b5d22dafce0a38
Tito Lins <tito@blinx.com.br>
2016-03-25 14:18:59 -0300

    test form field (not working -- formfield has no validation)

--- nscmr/forms.py
+++ nscmr/forms.py
@@ -4,37 +4,81 @@ from wtforms import (
     TextField,
     PasswordField,
     SubmitField,
-    ValidationError
+    ValidationError,
+    FieldList,
+    FormField
 )
 
 from wtforms.fields.html5 import DateField
 
-from wtforms.validators import input_required, email, equal_to, Optional
+from wtforms.validators import (
+    input_required,
+    email,
+    equal_to,
+    Optional,
+    Regexp)
 
 class LoginForm(Form):
     email = TextField(
-        'email',
+        'Email',
         validators=[
             input_required("Campo necessário!"),
             email("Email inválido!")])
 
-    password = PasswordField('senha', [input_required("Campo necessário!")])
+    password = PasswordField('Senha', [input_required("Campo necessário!")])
 
 
-class RegistrationForm(Form):
-    name = TextField('nome', validators=[input_required("Campo necessário!")])
+class ProfileForm(Form):
+    name = TextField('Nome', validators=[input_required("Campo necessário!")])
     email = TextField(
-        'email',
+        'Email',
         validators=[
             input_required("Campo necessário!"),
             email("Email inválido!")])
     dob = DateField(
-            "data de nascimento",
+            "Data de nascimento",
             validators=[Optional()])
     password = PasswordField(
-            'senha',
+            'Senha',
             validators=[
                 input_required("Campo necessário!"),
                 equal_to('confirm', message="As senhas precisam ser iguais")])
     confirm = PasswordField(
-            'confirme sua senha', [input_required("Campo necessário!")])
+            'Confirme sua senha', [input_required("Campo necessário!")])
+
+
+class AddressForm(Form):
+    name = TextField('Nome', validators=[input_required("Campo necessário!")])
+    street_address_1 = TextField('Nome',
+        validators=[input_required("Campo necessário!")])
+    street_address_2 = TextField('Complemento')
+    city = TextField('Cidade',validators=[input_required("Campo necessário!")])
+    zip_code = TextField('Cep',
+        validators=[
+            input_required("Campo necessário!"),
+            Regexp(r'[0-9]{5}-[0-9]{3}', message="Formato inválido")
+        ])
+    state = TextField('Estado', [input_required("Campo necessário!")])
+    # not needed for now..
+    #country
+    #phone??
+
+
+class RegistrationForm(Form):
+    name = TextField('Nome', validators=[input_required("Campo necessário!")])
+    email = TextField(
+        'Email',
+        validators=[
+            input_required("Campo necessário!"),
+            email("Email inválido!")])
+    dob = DateField(
+            "Data de nascimento",
+            validators=[Optional()])
+    password = PasswordField(
+            'Senha',
+            validators=[
+                input_required("Campo necessário!"),
+                equal_to('confirm', message="As senhas precisam ser iguais")])
+    confirm = PasswordField(
+            'Confirme sua senha', [input_required("Campo necessário!")])
+    # address and phone as custom fields, perhaps...
