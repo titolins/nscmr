277b8808e93aa0938f1c6bec0d0f746222d8b9bc
Tito Lins <tito@blinx.com.br>
2016-03-26 03:04:17 -0300

    dev code for creating categories and products

--- nscmr/__init__.py
+++ nscmr/__init__.py
@@ -11,7 +11,7 @@ from flask.ext.principal import (
 from instance.config import config_app
 from nscmr.admin import build_admin_bp
 from nscmr.admin.database import build_db
-from nscmr.admin.models.user import User
+from nscmr.admin.models import User
 
 def build_app():
     '''
@@ -65,24 +65,72 @@ from nscmr import views
 
 # dev code
 if __name__ == '__main__':
+    ########
+    # user #
+    ########
+
+    # create admin
     from werkzeug.security import generate_password_hash
-    from nscmr.admin.models.user import User
-    u = User.get_by_id('test@example.com')
+    from nscmr.admin.models import User, Category, Product
     user_content = {
-        '_id': 'test@example.com',
-        'name': 'dev user',
-        'password': generate_password_hash('123'),
+        '_id': 'admin@studioduvet.com',
+        'name': 'administrador',
+        'password': generate_password_hash('admin'),
         'roles': ['user','admin'],
     }
-    if u is None:
+    u = User.get_by_id('admin@studioduvet.com')
+    try:
+        u.id
+    except:
         u = User(user_content)
         u.set_defaults()
         u.insert()
+    # create regular users
     def insert_users():
+        user_content['roles'] = ['user']
         for i in range(1000):
             user_content['_id'] = 'test{}@example.com'.format(i)
             user = User(user_content)
             user.set_defaults()
             user.insert()
 
+    #########
+    # admin #
+    #########
+    # fields = ['name', 'parent', 'ancestors', 'thumbnail', 'header']
+    category_content = {
+        'name': '',
+        'parent': None,
+        'ancestors': [],
+        'thumbnail' : 'http://placehold.it/400x300',
+        'header': 'http://placehold.it/1400x400',
+    }
+    categories = ['Lençóis', 'Toalhas de mesa', 'Peseiras', 'Acessórios',
+        'Duvets', 'Colchas']
+    for category in categories:
+        category_content['name'] = category
+        c = Category(category_content.copy())
+        try:
+            c.insert()
+        except Exception as e:
+            print(e)
+
+    def insert_products(n=9):
+        categories = Category.get_all()
+        for category in categories:
+            for i in range(9):
+                description = (
+                        'FEITO COM PERCAL 7000 FIOS DA ÍNDIA ORIENTAL',
+                        'IDEAL PARA CASAS DE CAMPO, SÍTIOS, FAZENDAS ETC.')
+                product_content = {
+                    'name': 'Lençol com estampa florida c/ tecido importado',
+                    'description': description,
+                    'price': 450.00,
+                    'size': "3.50m x 2.50m",
+                    'thumbnail': "http://placehold.it/400x300",
+                    'background': "http://placehold.it/1920x1080",
+                    'category': category
+                }
+                p = Product(product_content)
+                p.insert()
 
