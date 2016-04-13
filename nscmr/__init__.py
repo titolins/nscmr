1b98d0dca6610dee282dc8d5756b4b6aac05df5b
Tito Lins <tito@blinx.com.br>
2016-04-13 17:56:21 -0300

    removed dev code

--- nscmr/__init__.py
+++ nscmr/__init__.py
@@ -88,14 +88,10 @@ from nscmr import views
 
 # dev code
 if __name__ == '__main__':
-    ########
-    # user #
-    ########
 
     # create admin
     from werkzeug.security import generate_password_hash
-    from nscmr.admin.models import User, Category, Product
-    from nscmr.admin.helper import slugify
+    from nscmr.admin.models import User
     user_content = {
         '_id': 'admin@studioduvet.com',
         'name': 'administrador',
@@ -109,59 +105,4 @@ if __name__ == '__main__':
         u = User(user_content)
         u.set_defaults()
         u.insert()
-    # create regular users
-    def insert_users():
-        user_content['roles'] = ['user']
-        for i in range(1000):
-            user_content['_id'] = 'test{}@example.com'.format(i)
-            user = User(user_content)
-            user.set_defaults()
-            user.insert()
-
-    #########
-    # admin #
-    #########
-    # fields = ['name', 'parent', 'ancestors', 'thumbnail', 'header']
-    category_content = {
-        'name': '',
-        'parent': None,
-        'ancestors': [],
-        'base_img' : 'http://placehold.it/400x300',
-    }
-    categories = ['Lençóis', 'Toalhas de mesa', 'Peseiras', 'Acessórios',
-        'Duvets', 'Colchas']
-    for category in categories:
-        category_content['name'] = category
-        category_content['permalink'] = slugify(category)
-        c = Category(category_content.copy())
-        try:
-            c.insert()
-        except Exception as e:
-            print(e)
-
-    def insert_products(n=9):
-        categories = Category.get_all()
-        products = [
-            '{} com estampa florida c/ tecido importado',
-            '{} sem estampa c/ tecido importado',
-            '{} com estampa customizada c/ tecido importado',
-        ]
-        for category in categories:
-            for product in products:
-                description = (
-                        'FEITO COM PERCAL 7000 FIOS DA ÍNDIA ORIENTAL',
-                        'IDEAL PARA CASAS DE CAMPO, SÍTIOS, FAZENDAS ETC.')
-                name = product.format(category['name'])
-                product_content = {
-                    'name': name,
-                    'description': description,
-                    'price': 450.00,
-                    'size': "3.50m x 2.50m",
-                    'thumbnail': "http://placehold.it/400x300",
-                    'background': "http://placehold.it/1920x1080",
-                    'category_id': category['_id'],
-                    'permalink': slugify(name),
-                }
-                p = Product(product_content)
-                p.insert()
 
