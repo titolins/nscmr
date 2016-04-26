5f81a3fff5d6aa6b99a460636ebba0f4f982c560
Tito Lins <tito@blinx.com.br>
2016-04-26 17:47:18 -0300

    product and variant from_form() implemented. user now has objectId and email

--- nscmr/admin/models/__init__.py
+++ nscmr/admin/models/__init__.py
@@ -13,7 +13,8 @@ class User(Document):
     '''User class
     '''
     __collection__ = 'users'
-    fields = ['name','dob','roles','addresses','wishlist','orders','cart']
+    fields = ['email', 'name', 'dob', 'roles', 'addresses', 'wishlist',
+            'orders', 'cart']
     defaults = {
         'roles': ['user'],
         'addresses': [],
@@ -23,12 +24,14 @@ class User(Document):
     }
     # required fields
     required_fields = ['name', 'password']
+    indexes = {
+        'email': { 'unique': True },
+        'email': ASCENDING,
+    }
 
     @staticmethod
     def from_form(form_data):
-        # use email as _id and delete email so we don't get repeated fields
-        form_data['_id'] = form_data['email'].lower()
-        del(form_data['email'])
+        form_data['email'] = form_data['email'].lower()
 
         # converts date object to datetime, as pymongo only support the latter
         if form_data['dob'] is not None:
@@ -66,6 +69,10 @@ class User(Document):
     def check_password(self, password):
         return check_password_hash(self._content['password'], password)
 
+    @classmethod
+    def get_by_email(cls, email, to_obj=False):
+        return cls._get_one(to_obj, { "email": email })
+
 
 class Category(SlugDocument):
 
@@ -95,36 +102,52 @@ class Category(SlugDocument):
 
 class Product(SlugDocument):
     __collection__ = 'products'
-    fields = ['name', 'description', 'category', 'permalink']
+    # category should contain name and _id
+    fields = ['name', 'description', 'category', 'meta_description',
+            'permalink']
 
     indexes = {
         'name': { 'unique': True },
+        'permalink': { 'unique': True },
         'permalink': ASCENDING,
     }
 
     @classmethod
     def get_by_category(cls, category_id, to_obj=False):
-        return cls._get_many(to_obj, { "category._id": category_id })
+        return cls._get_many(to_obj, { "category._id": str(category_id) })
 
     @staticmethod
     def from_form(form_data):
-        return NotImplemented
+        form_data['name'] = form_data['name'].lower()
+        form_data['permalink'] = slugify(form_data['name'])
+        return Product(form_data)
 
     def get_category(self):
         return Category.get_by_id(self._content['category']['_id'])
 
+    def get_name(self):
+        return self._content['name'].capitalize()
+
+    def get_variants(self, to_obj=True):
+        return Variant._get_many(to_obj, { "product_id": self.id })
+
 
 class Variant(Document):
     __collection__ = 'variants'
-    fields = ['product', 'images', 'sku', 'price' 'attributes']
+    fields = ['product_id', 'images', 'sku', 'price', 'attributes', 'quantity']
     # images must have the following format { 'big': <url>, 'thumb': <url> }
     # attributes should contain: size, color, etc.. and any other
     # later on though
-    # product should contain 'name' and '_id'
 
     @staticmethod
     def from_form(form_data):
-        return NotImplemented
+        form_data['sku'] = form_data['sku'].upper()
+        if 'attributes' in form_data:
+            attrs = {}
+            for k,v in form_data['attributes'].items():
+                attrs[k.lower()] = form_data['attributes'][k].lower()
+            form_data['attributes'] = attrs
+        return Variant(form_data)
 
     def get_product(self):
         return Product.get_by_id(self._content['product_id'])
