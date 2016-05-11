fd8ff1a0c01bc4d79d01da8e6e0c5ce0ae8b352b
Tito Lins <tito@blinx.com.br>
2016-05-11 17:47:07 -0300

    category and users using same edit js

--- nscmr/admin/static/js/category.js
+++ nscmr/admin/static/js/category.js
@@ -64,48 +64,4 @@ $(document).ready(function() {
     },
   });
 
-  $("#datatable tbody").on('click', 'td', function(e) {
-    if ($(e.target).hasClass('edit-attribute')) {
-      var cell = $(e.target).closest('td');
-      var value = cell.find('.value').text();
-      var row = cell.closest('tr');
-      cell.addClass('edited');
-      // hide current cell content
-      cell.children().toggleClass('hidden');
-      if (cell.find('input').length === 0 && cell.find('select').length === 0) {
-        if (cell.hasClass('parent')) {
-          editField = editParentField.clone();
-          // get all select options and disable itself
-          var cId = row.find('#id').text();
-          var options = $.map(editField.find("option"), function(item) {
-            if ($(item).attr('value').split('_')[0] === cId) $(item).attr('disabled', true);
-            return $(item).attr('value');
-          });
-          // find the current category and set it
-          var choice = null;
-          options.some(function(option) {
-            if (option.split('_')[1] === value) {
-              choice = option;
-            }
-          });
-        } else if (cell.hasClass('name')) {
-          editField = editNameField.clone();
-        } else if (cell.hasClass('base_img')) {
-          editField = editBaseImgField.clone();
-        } else if (cell.hasClass('meta_description')) {
-          editField = editMetaField.clone();
-        }
-        cell.append(editField);
-        cell.append($('<a href="#"><span class="undo-edit glyphicon glyphicon-remove"></span></a>'));
-        if (cell.hasClass('parent')) editField.select2({}).val(choice).trigger('change');
-        else if (!cell.hasClass('base_img')) editField.val(value);
-      }
-      return false;
-    } else if ($(e.target).hasClass('undo-edit')) {
-      var cell = $(e.target).closest('td');
-      cell.removeClass('edited');
-      cell.children().toggleClass('hidden');
-      return false;
-      }
-  });
 });
--- nscmr/admin/static/js/user.js
+++ nscmr/admin/static/js/user.js
@@ -8,6 +8,40 @@ function getDeleteData() {
     return { users: selectedCategories };
 }
 
+function getEditData() {
+  var edited = $('.edited');
+  var users = [];
+  edited.each(function() {
+    buildUserData($(this).closest('tr'), $(this), users);
+  });
+  console.log(users);
+  var form = new FormData();
+  form.append('users', JSON.stringify(users));
+  return form;
+};
+
+function buildUserData(row, cell, data) {
+    var colName = cell.attr('id');
+    var value;
+    if (colName == 'dob') value = new Date( cell.find('input')[0].valueAsDate ).toISOString().substr(0,10);
+    else value = cell.find('input').first().val()
+    var uId = table.row(row).data()[1];
+    var inserted = false;
+    for (i = 0; i < data.length; i++) {
+      if (data[i].id == uId) {
+        data[i][colName] = value;
+        inserted = true;
+      }
+    }
+    if (!inserted) {
+      var user = {
+        id: uId,
+      };
+      user[colName] = value;
+      data.push(user);
+    }
+}
+
 $(document).ready(function() {
   $("#is_admin").change(function() {
     if ($(this).is(':checked')) {
