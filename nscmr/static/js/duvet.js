17036969a204fd7759d23c8d173f5deaacbd72de
Tito Lins <tito@blinx.com.br>
2016-05-19 20:40:54 -0300

    added animation to navbar dropdown

--- nscmr/static/js/duvet.js
+++ nscmr/static/js/duvet.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $(".trigger-menu").on('click', function() {
     var target = $($(this).data('target'));
     if (this.id == "sidebar-btn") {
-      $(this).toggleClass('fixed');
+      //$(this).toggleClass('fixed');
       var buyMenu = $("#buy-menu");
       if (buyMenu.hasClass('open')) {
         buyMenu.removeClass('open');
@@ -12,5 +12,13 @@ $(document).ready(function() {
     $(this).toggleClass('active');
     target.toggleClass('open');
     return false;
+  });
+  $(".item-dropdown-toggle").on('click', function() {
+    var target = document.getElementById($(this).data('toggle'));
+    var rect = this.getBoundingClientRect();
+    var left = rect.left - (target.offsetWidth - (rect.right - rect.left));
+    target.style.right = "" + rect.right + "px";
+    target.style.left = "" + left + "px";
+    $(target).toggleClass('open');
   });
 });
