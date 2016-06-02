bd58c631a33d469b108e52c0eacc0459e742a78a
Tito Lins <tito@blinx.com.br>
2016-06-02 19:15:37 -0300

    fixed sidebar menu btn not fixing on sidebar open

--- nscmr/static/js/duvet.js
+++ nscmr/static/js/duvet.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $(".trigger-menu").on('click', function() {
     var target = $($(this).data('target'));
     if (this.id == "sidebar-btn") {
-      //$(this).toggleClass('fixed');
+      $(this).find('i').toggleClass('fixed');
       var buyMenu = $("#buy-menu");
       if (buyMenu.hasClass('open')) {
         buyMenu.removeClass('open');
