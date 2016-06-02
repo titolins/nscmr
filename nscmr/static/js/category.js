6fe8d404b66631d467b42c6b6164a591a0b1b6cf
Tito Lins <tito@blinx.com.br>
2016-06-02 18:15:35 -0300

    angular cart ;)

--- nscmr/static/js/category.js
+++ nscmr/static/js/category.js
@@ -1,5 +1,19 @@
-$(document).ready(function() {
-  $('.cart-btn').on('click', function() {
-    
-  });
-});
+angular.module('angularApp').controller("CartController", ["$scope","$http", function($scope, $http) {
+  $scope.addToCart = function(variantId) {
+    $http({
+      method: 'POST',
+      url: addToCartUri,
+      data: { 'variant_id': variantId },
+      headers: {
+        "X-CSRFToken": csrfToken,
+        "Content-Type": "application/json;utf-8"
+      }
+    }).then(function successCallback(response) {
+      console.log(response);
+    }, function errorCallback(response) {
+      console.log(response);
+    });
+    return false;
+  };
+}]);
+
