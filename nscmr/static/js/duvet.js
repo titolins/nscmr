ae255d357d6875529404b8ff63f473e4ae4f0ffc
Tito Lins <tito@blinx.com.br>
2016-06-04 22:02:16 -0300

    refactor dropdowns toggle

--- nscmr/static/js/duvet.js
+++ nscmr/static/js/duvet.js
@@ -1,24 +1,19 @@
 $(document).ready(function() {
   $(".trigger-menu").on('click', function() {
     var target = $($(this).data('target'));
-    if (this.id == "sidebar-btn") {
-      $(this).find('i').toggleClass('fixed');
-      var buyMenu = $("#buy-menu");
-      if (buyMenu.hasClass('open')) {
-        buyMenu.removeClass('open');
-        $("#buy-btn").removeClass('active');
-      }
-    }
-    $(this).toggleClass('active');
-    target.toggleClass('open');
+    if (target.hasClass('open')) $('.open').removeClass('open');
+    else target.toggleClass('open');
     return false;
   });
   $(".item-dropdown-toggle").on('click', function() {
     var target = document.getElementById($(this).data('toggle'));
+    /*
     var rect = this.getBoundingClientRect();
     var left = rect.left - (target.offsetWidth - (rect.right - rect.left));
     target.style.right = "" + rect.right + "px";
     target.style.left = "" + left + "px";
+    */
+    //$('.open').removeClass('open');
     $(target).toggleClass('open');
   });
 });
