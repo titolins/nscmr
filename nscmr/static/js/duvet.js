a80c24ccdf2fce43a8b8b5981c5eebfa69af1f2d
Tito Lins <tito@blinx.com.br>
2016-06-07 14:43:25 -0300

    changed dropdown positions

--- nscmr/static/js/duvet.js
+++ nscmr/static/js/duvet.js
@@ -7,13 +7,26 @@ $(document).ready(function() {
   });
   $(".item-dropdown-toggle").on('click', function() {
     var target = document.getElementById($(this).data('toggle'));
-    /*
-    var rect = this.getBoundingClientRect();
-    var left = rect.left - (target.offsetWidth - (rect.right - rect.left));
-    target.style.right = "" + rect.right + "px";
-    target.style.left = "" + left + "px";
-    */
-    //$('.open').removeClass('open');
+    if (target.id.startsWith('categories')) {
+      if (window.innerWidth < 768) target.style.left = 0;
+      else {
+        var rect = this.getBoundingClientRect();
+        var left = rect.right - (target.offsetWidth - (rect.left - rect.right));
+        target.style.left = "" + rect.left + "px";
+        target.style.right = "" + rect.right + "px";
+        //$('.open').removeClass('open');
+      }
+    }
+    else {
+      if (window.innerWidth < 768) target.style.right = 0;
+      else {
+        var rect = this.getBoundingClientRect();
+        var left = rect.left - (target.offsetWidth - (rect.right - rect.left));
+        target.style.right = "" + rect.right + "px";
+        target.style.left = "" + left + "px";
+        //$('.open').removeClass('open');
+      }
+    }
     $(target).toggleClass('open');
   });
 });
