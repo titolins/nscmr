34fb4a8ade93ffde37fd6a54929251a5b5cf3bc5
Tito Lins <tito@blinx.com.br>
2016-06-07 20:13:03 -0300

    deleted angular_config file

--- nscmr/static/js/angular_config.js
+++
@@ -1,5 +0,0 @@
-angular.module('angularApp', []).config(['$interpolateProvider', function($interpolateProvider) {
-  $interpolateProvider.startSymbol('{a');
-  $interpolateProvider.endSymbol('a}');
-}]);
-
