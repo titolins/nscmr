79c3463f32b9001217781fd81397e72950206aaa
Tito Lins <tito@blinx.com.br>
2016-06-10 20:20:27 -0300

    fixed a couple of funcionalities. wishlist now using angular as well

--- nscmr/static/js/userprofile.js
+++ nscmr/static/js/userprofile.js
@@ -3,8 +3,13 @@ angular.module('angularApp', ['ui.mask'])
   $interpolateProvider.startSymbol('{a');
   $interpolateProvider.endSymbol('a}');
 }]).controller("UserProfileController", ["$scope","$http", function($scope, $http) {
+  $scope.user = {},
+  $scope.cart = [],
+  $scope.addresses = [],
+  $scope.wishlist = [];
   getUser();
   getCart();
+  getWishlist();
 
   $scope.addToWishlist = function(variantId) {
     var data = { 'variant_id': variantId };
@@ -49,6 +54,7 @@ angular.module('angularApp', ['ui.mask'])
       url: getUserUri,
     }).then(function successCallback(response) {
       console.log(response.data);
+      response.data['dob'] = new Date(response.data['dob']);
       $scope.initialUser = response.data;
       $scope.user = angular.copy($scope.initialUser);
       $scope.initialAddresses = response.data['addresses'];
@@ -58,16 +64,29 @@ angular.module('angularApp', ['ui.mask'])
     });
   };
 
+  function getWishlist() {
+    $http({ url: getWishlistUri }).then(function success(response) {
+      console.log(response);
+      $scope.wishlist = response.data;
+      console.log($scope.wishlist);
+    }, function error(response) {
+      console.log(response);
+    });
+  };
+
   $scope.toggleEdit = function($event) {
     var target = $event.currentTarget;
     // if we are cancelling the edit, we simply reset the cart (redrawing the
     // template)
-    if (target.classList.contains('cancel-cart')) $scope.cart = angular.copy($scope.initialCart);
-    else if (target.classList.contains('cancel-name')) $scope.user['name'] = $scope.initialUser['name'];
+    var toggle = true;
+    if (target.classList.contains('cancel-cart')) {
+      toggle = false;
+      $scope.cart = angular.copy($scope.initialCart);
+    } else if (target.classList.contains('cancel-name')) $scope.user['name'] = $scope.initialUser['name'];
     else if (target.classList.contains('cancel-email')) $scope.user['email'] = $scope.initialUser['email'];
     else if (target.classList.contains('cancel-dob')) $scope.user['dob'] = $scope.initialUser['dob'];
     else if (target.classList.contains('cancel-addresses')) $scope.addresses = angular.copy($scope.initialAddresses);
-    else {
+    if (toggle) {
       var field = target.parentNode;
       while (!field.classList.contains('field')) field = field.parentNode;
       field.getElementsByClassName('field-value')[0].classList.toggle('hidden');
@@ -75,12 +94,13 @@ angular.module('angularApp', ['ui.mask'])
     }
   }
 
+
   $scope.removeItem = function(var_id) {
     data = {
       'id': var_id,
       'quantity': 0
     }
-    edit(data);
+    editCart(data);
   };
 
   $scope.confirmEdit = function(item) {
@@ -88,10 +108,31 @@ angular.module('angularApp', ['ui.mask'])
       'id': item['_id'],
       'quantity': item['quantity']
     }
-    edit(data);
+    editCart(data);
   };
 
-  function edit(data) {
+  $scope.removeFromWishlist = function(var_id) {
+    data = { 'id': var_id };
+    console.log(data);
+    $http({
+      method: 'POST',
+      url: removeFromWishlistUri,
+      data: data,
+      headers: {
+        "X-CSRFToken": csrfToken,
+        "Content-Type": "application/json;utf-8"
+      }
+    }).then(function success(response) {
+      console.log(response);
+      alert(response.data);
+      getWishlist();
+    }, function error(response) {
+      console.log(response);
+      alert(response.data);
+    });
+  };
+
+  function editCart(data) {
     console.log(data);
     $http({
       method: 'POST',
@@ -142,7 +183,10 @@ angular.module('angularApp', ['ui.mask'])
     var userFields = [ 'name', 'email', 'dob' ];
     var user = {};
     userFields.forEach(function(field) {
+      console.log($scope.initialUser[field]);
+      console.log($scope.user[field]);
       if ($scope.user[field] != $scope.initialUser[field]) user[field] = $scope.user[field];
+      if (field === 'dob') user[field] = $scope.parseDate(user[field]);
     });
     console.log(user);
     sendPost(editUserUri, user, function(response) {
@@ -182,11 +226,26 @@ angular.module('angularApp', ['ui.mask'])
     var apiUri = 'http://api.postmon.com.br/v1/cep/' + $scope.form.zip_code;
     $http({url:apiUri}).then(function success(response) {
       $scope.form.street_address_1 = response.data['logradouro'];
-      $scope.form.street_address_2 = response.data['bairro'];
+      $scope.form.neighbourhood = response.data['bairro'];
       $scope.form.city = response.data['cidade'];
       $scope.form.state = response.data['estado'];
     }, function error(response) {});
 
+  }
+
+  $scope.parseDate = function(date) {
+    if (date !== undefined) {
+      var year  = date.getFullYear().toString(),
+          month = (date.getMonth()+1).toString(),
+          day   = date.getDate().toString();
+      return (day[1]?day:'0'+day[0])+'/'+(month[1]?month:'0'+month[0])+'/'+year;
+    }
+    return "";
+  };
+
+  $scope.buildLink = function(catPermalink, prodPermalink, varId) {
+    var baseUrl = '/catalogo/';
+    return baseUrl + catPermalink + '/' + prodPermalink + '/' + varId;
   }
 
 }]);
