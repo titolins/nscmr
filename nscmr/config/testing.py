e77cc893802f4c7fe676dfe38e5ac88ed0241740
Tito Lins <tito@blinx.com.br>
2016-08-01 14:40:54 -0300

    changed fb app id and secret for testing

--- nscmr/config/testing.py
+++ nscmr/config/testing.py
@@ -18,7 +18,7 @@ MUNDIPAGG_ENDPOINT = 'https://sandbox.mundipaggone.com/'
 
 GOOGLE_CLIENT_ID = "883320219445-ejvh51karin9m6dg9mbql56vjn0vp2ls.apps.googleusercontent.com"
 
-# dev (test app) settings (change for production)
-FB_APP_ID = "1050879885027789"
-FB_APP_SECRET = "826f7fca7e04f05e9d4bae3de5a8e7a2"
+# server test app settings (change for production)
+FB_APP_ID = "1053748188074292"
+FB_APP_SECRET = "654c8683322a23800b4645b5ede3c832"
 FB_APP_SCOPE = "public_profile,email"
