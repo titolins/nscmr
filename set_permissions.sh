2cda9d5843ffc6fedf253a3c623722642234c27b
Tito Lins <tito@blinx.com.br>
2016-08-10 19:23:07 -0300

    changed get_interactive name and modified set_permission to not alter .sh files

--- set_permissions.sh
+++ set_permissions.sh
@@ -4,6 +4,6 @@ APP_DIR=/var/www/nscmr
 
 chgrp -R www-data $APP_DIR
 find $APP_DIR -type d -exec chmod 775 {} \;
-find $APP_DIR -type f -exec chmod 664 {} \;
+find $APP_DIR -type f -name "*[^*.sh]" -exec chmod 664 {} \;
 
 
