eef691dc63b8ecdcb1d7578529452bbe43bc93aa
Tito Lins <tito@blinx.com.br>
2016-08-10 18:51:09 -0300

    fixed typo

--- set_permissions.sh
+++ set_permissions.sh
@@ -3,7 +3,7 @@
 $APP_DIR='/var/www'
 
 chgrp -R www-data $APP_DIR
-find $APP_DIR -type d -exec chmode 775 {} \;
-find $APP_DIR -type f -exec chmode 664 {} \;
+find $APP_DIR -type d -exec chmod 775 {} \;
+find $APP_DIR -type f -exec chmod 664 {} \;
 
 
