7b56a6e272928baafa83db928db463d853d74340
Tito Lins <tito@blinx.com.br>
2016-08-26 20:33:23 -0300

    added sudo to docker cmds

--- startdocker.sh
+++ startdocker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-docker run -v dbdata:/var/lib/mongodb -v $SSH_AUTH_SOCK:/ssh-agent --env \
+sudo docker run -v dbdata:/var/lib/mongodb -v $SSH_AUTH_SOCK:/ssh-agent --env \
     SSH_AUTH_SOCK=/ssh-agent -d -p 80:80 --name nscmr nscmr
 
-docker exec -it nscmr service mongod start
+sudo docker exec -it nscmr service mongod start
 
 #sudo docker exec -it nscmr rm -rf /app/nscmr/static/.webassets-cache
 
