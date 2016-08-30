b57c1e1c45f4993be2fb3eb8ca697fc899353c5b
Tito Lins <tito@blinx.com.br>
2016-08-30 00:21:33 -0300

    fixing case insensitive images

--- nscmr/admin/helper/__init__.py
+++ nscmr/admin/helper/__init__.py
@@ -23,17 +23,21 @@ def slugify(text, delim=u'-'):
     return delim.join(result)
 
 def make_thumb(image, path, size=(350,480)):
-    full_img_path = os.path.join(path, image)
+    filename = image.split('.',1)[0]
     ext = image.rsplit('.',1)[1]
+    image = '{}.{}'.format(filename, ext)
     if ext.lower() == 'jpg':
         ext = 'jpeg'
-    filename = image.split('.',1)[0]
+    full_img_path = os.path.join(path, image)
     thumb_filename = '{}_{}.{}'.format(filename, 'thumbnail', ext)
+    print(thumb_filename)
     output_file = os.path.join(path, thumb_filename)
     try:
         im = Image.open(full_img_path)
+        print('ok')
         im.thumbnail(size)
         im.save(output_file, ext)
+        print('ok2')
         return thumb_filename
     except IOError as e:
         print('Erro criando thumbnail de {}'.format(image))
