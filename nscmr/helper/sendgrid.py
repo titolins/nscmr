f700e758e15a28c60ccbb28a0e4fea7b5fc7929d
Tito Lins <tito@blinx.com.br>
2016-09-09 18:28:41 -0300

    built confirmation email

--- nscmr/helper/sendgrid.py
+++ nscmr/helper/sendgrid.py
@@ -1,7 +1,22 @@
 import sendgrid
 from sendgrid.helpers.mail import *
 
-from flask import current_app as app
+from flask import current_app as app, render_template
+
+TEXT_EMAIL_CONTENT = '''
+        Prezado(a) {}\n,
+        Sua compra n. {} foi concluída com sucesso.\n
+        Agradecemos por nos escolher! Para acompanhar o seu andamento, entre
+        na sua conta no site do StudioDuvet e acesse seu perfil clicando no
+        link com seu nome na barra superior.\n
+        Agradecemos novamente pela preferência!\n
+        Um grande abraço,\n
+        Studio Duvet
+    '''.format(
+            user['name'].capitalize(),
+            order['reference']
+        )
+
 
 def send_confirmation_mail(user, order):
     sg = sendgrid.SendGridAPIClient(apikey=app.config.get('SENDGRID_API_KEY'))
@@ -12,13 +27,15 @@ def send_confirmation_mail(user, order):
     to_email = Email(
         email=user.email,
         name=user.name)
-    html, text = build_confirmation_mail_content(user.name, order.reference)
+
     text_content = Content(
         type="text/plain",
-        value=text)
+        value=TEXT_EMAIL_CONTENT)
+
     html_content= Content(
         type="text/html",
-        value=html)
+        value=render_template(
+            'confirmationemail.html', user=user, order=order))
 
     mail = Mail(
         from_email=from_email,
@@ -34,88 +51,4 @@ def send_confirmation_mail(user, order):
     print(r.body)
     print(r.headers)
 
-
-def build_confirmation_mail_content(name, order_reference):
-    domain = "http://45.55.162.155"
-    logo = "{}/static/imgs/logo_studio.png".format(domain)
-    def build_html():
-        return \
-            '''
-                <html>
-                    <body>
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <td>
-                                        <img src="{}">
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td style="padding-top: 20px">
-                                        <h3 style="font-weight: lighter;
-                                            text-transform: uppercase;">
-                                                parabéns pela sua compra!
-                                        </h3>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Prezado Sr(a). {},</td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <p>
-                                            Sua compra n. <b>{}</b> foi
-                                            concluída com sucesso.<br>
-                                            Para acompanhar o seu andamento,
-                                            volte para o site do <a href="{}">
-                                            StudioDuvet</a> e entre na sua
-                                            conta.<br>
-                                            Uma vez logado, acesse o seu perfil
-                                            clicando no link com o seu nome na
-                                            barra superior e selecionando
-                                            perfil no menu.<br>
-                                            Ao selecionar suas compras, você
-                                            poderá confirmar os produtos
-                                            adquiridos, assim como o endereço
-                                            de entrega e o seu andamento.<br>
-                                        </p>
-                                        <p>
-                                            Agradecemos novamente pela
-                                            preferência!<br>
-                                            Um grande abraço,
-                                            Studio Duvet
-                                        </p>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </body>
-                </html>
-            '''.format(
-                logo,
-                name,
-                order_reference,
-                domain
-            )
-    def build_text():
-        return \
-            '''
-            PARABÉNS PELA SUA COMPRA!\n
-            Prezado Sr(a). {},\n
-            Sua compra n. {} foi concluída com sucesso.\n
-            Para acompanhar o seu andamento, volte para o site do StudioDuvet\n
-            e entre na sua conta.
-            Uma vez logado, acesse o seu perfil clicando no link com o seu \n
-            nome na barra superior e selecionando perfil no menu.\n
-            Ao selecionar suas compras, você poderá confirmar os produtos \n
-            adquiridos, assim como o endereço de entrega e o seu andamento.\n
-
-            Agradecemos novamente pela preferência!\n
-            Um grande abraço,\n
-            Studio Duvet
-            '''.format(
-                name,
-                order_reference,
-            )
-    return (build_html(), build_text())
 
